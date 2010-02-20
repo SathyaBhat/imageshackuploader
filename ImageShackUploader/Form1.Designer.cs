bb1deb8ad2ed6fa77d051f0aea9904a9aa5acd96
Sathya <sathya@sathyasays.com>
2010-02-20 20:51:52 +0530

    AddedCopy to thumbnail link

---
+++ ImageShackUploader/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ImageShackUploader
+{
+    partial class frmISUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmISUpload));
+            this.niSnap = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SelModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyThumbnailLinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // niSnap
+            // 
+            this.niSnap.ContextMenuStrip = this.contextMenuStrip1;
+            this.niSnap.Icon = ((System.Drawing.Icon)(resources.GetObject("niSnap.Icon")));
+            this.niSnap.Visible = true;
+            this.niSnap.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.niSnap_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.SelModeToolStripMenuItem,
+            this.copyThumbnailLinkToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(189, 92);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // SelModeToolStripMenuItem
+            // 
+            this.SelModeToolStripMenuItem.Name = "SelModeToolStripMenuItem";
+            this.SelModeToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.SelModeToolStripMenuItem.Text = "Selection Mode";
+            this.SelModeToolStripMenuItem.Click += new System.EventHandler(this.SelModeToolStripMenuItem_Click);
+            // 
+            // copyThumbnailLinkToolStripMenuItem
+            // 
+            this.copyThumbnailLinkToolStripMenuItem.Name = "copyThumbnailLinkToolStripMenuItem";
+            this.copyThumbnailLinkToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.copyThumbnailLinkToolStripMenuItem.Text = "Copy Thumbnail Link";
+            this.copyThumbnailLinkToolStripMenuItem.Click += new System.EventHandler(this.copyThumbnailLinkToolStripMenuItem_Click);
+            // 
+            // frmISUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmISUpload";
+            this.Text = "ImageShack Uploader";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.frmISUpload_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon niSnap;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SelModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyThumbnailLinkToolStripMenuItem;
+    }
+}
+
---
+++ ImageShackUploader/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Brycestrosoft.ImageShackAPIWrapper;
+
+namespace ImageShackUploader
+/* Simple capture desktop and upload to Imageshack tool 
+ * By Sathyajith Bhat - http://sathyabh.at/
+ * Drop a mail - sathya@sathyasays.com
+ * Uses Bryce's C# Wrapper for Imageshack API ( http://www.codeemporium.com/2009/06/14/dot-net-c-sharp-wrapper-for-the-imageshack-xml-api/ )
+ * Thanks to Omkarnath for coding the grab only selection mode logic http://intelomkar.wordpress.com/2009/12/21/screencapture-library/
+ * Legal stuff - You're free to use this app. Sending me a mail on how you've used it would be helpful :)
+ * And yes -  I am liable for nothing that you do. Something screws up - don't blame me. */
+{
+    public partial class frmISUpload : Form
+    {
+       
+        Image imgScreenCap;
+        string strThumbnailLink = "";
+        string strFullImageLink="";
+        public frmISUpload()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void niSnap_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+
+            if (CaptureScreen())
+                UploadImage();
+
+        }
+
+        public bool CaptureScreen()
+        {
+            string strFilename = "upload.jpg"; // chosen an arbitrary upload.jpeg filename
+            // TO DO: Create a fancy thingamajig to randomize file name            
+            if (Clipboard.ContainsImage())
+            {
+
+                imgScreenCap = Clipboard.GetImage();
+                imgScreenCap.Save(strFilename, ImageFormat.Jpeg);
+                System.Threading.Thread.Sleep(1000);
+                imgScreenCap.Dispose();
+                //   niSnap.BalloonTipText = "Image saved";
+                //  niSnap.ShowBalloonTip(5000);
+                return true;
+            }
+            else
+            {
+                niSnap.BalloonTipText ="No image in clipboard to upload";
+                niSnap.ShowBalloonTip(10000);
+                return false;
+            }
+           
+        }
+
+        public bool UploadImage()
+        {
+            UploadedImageDetails ImageDetails;
+            
+            IImageShackUploader uploader = new StandardImageShackUploader();
+            try
+            {
+
+                ImageDetails = uploader.UploadImage("upload.jpg");
+                niSnap.BalloonTipText = ("Image has been uploaded to ImageShack and link has been copied to clipboard");
+                Clipboard.SetText(ImageDetails.ImageLink.ToString());
+                strFullImageLink = ImageDetails.ImageLink.ToString();
+                strThumbnailLink = ImageDetails.ThumbLink.ToString();
+                niSnap.ShowBalloonTip(500000);
+                GC.Collect();
+                return true;
+            }
+            catch (Exception excError)
+            {
+                MessageBox.Show("Error: " +  excError.Message.ToString());
+                GC.Collect();
+                return false;
+            }
+
+        }
+
+        private void frmISUpload_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Close();
+        }
+
+        private void SelModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            this.ShowInTaskbar = true;
+            screenCapture.screenCapture sc = new screenCapture.screenCapture();
+            sc.ImageFormat = System.Drawing.Imaging.ImageFormat.Jpeg; //jpeg is default
+            sc.imageName = "upload.jpg"; //default is img
+            //above 2 lines are optional
+            sc.showCanvas();
+            System.Threading.Thread.Sleep(1000);
+            UploadImage();
+        }
+
+        private void copyThumbnailLinkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (strThumbnailLink.Equals(""))
+            {
+                niSnap.BalloonTipText = "Nothing to copy!";
+                niSnap.ShowBalloonTip(10000);
+            }
+            else
+            { Clipboard.SetText(strThumbnailLink); }
+
+        }
+
+
+
+ 
+    }
+
+
+    
+
+      
+    }
+
