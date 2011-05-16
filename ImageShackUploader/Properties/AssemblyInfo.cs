42074bdb05b75d5cad2f84beee55c116701c1af9
Sathya <sathya@sathyasays.com>
2011-05-16 22:27:53 +0530

    Updated description

--- ImageShackUploader/Properties/AssemblyInfo.cs
+++ ImageShackUploader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageShackUploader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ImgurUploader")]
+[assembly: AssemblyDescription("Simple, no frills upload to imgur application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sathya")]
-[assembly: AssemblyProduct("ImageShackUploader")]
+[assembly: AssemblyProduct("ImgurUploader")]
 [assembly: AssemblyCopyright("Copyright © Sathya 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
